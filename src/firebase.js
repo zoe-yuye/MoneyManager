760f0e507406b7c9d0b37390ce9e848f2199e0a7
zhengyuye <513557074@qq.com>
2024-01-15 20:31:53 +1300

    test

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,6 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDphjj5nD7B52bGv38F1BMV4TcUZUsy2O4",
-  authDomain: "money-app-a59bf.firebaseapp.com",
-  projectId: "money-app-a59bf",
-  storageBucket: "money-app-a59bf.appspot.com",
-  messagingSenderId: "751721053608",
-  appId: "1:751721053608:web:0a78762e2fe0ace246b19d"
 };
 
 initializeApp(firebaseConfig);
